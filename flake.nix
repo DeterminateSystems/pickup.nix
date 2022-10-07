4961b83a7fd9e90acb6198623f2e02e9f0ba7753
Graham Christensen <graham@grahamc.com>
2022-10-07 10:25:11 -0400

    simplify for non-flake users

--- flake.nix
+++ flake.nix
@@ -1,26 +1,10 @@
 {
-  inputs = {
-    nix-netboot-serve = {
-      url = "github:DeterminateSystems/nix-netboot-serve";
-      inputs.nixpkgs.follows = "nixpkgs";
+  outputs = { ... }: {
+    nixosModules = {
+      hook = import ./modules/hook.nix;
+
+      stub-aws-aarch64 = import ./modules/stub-aws-aarch64.nix;
     };
   };
-
-  outputs =
-    { self
-    , nixpkgs
-    , nix-netboot-serve
-    }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-    in
-    {
-      nixosModules = {
-        hook = ./modules/hook.nix;
-
-        stub-aws-aarch64 = ./modules/stub-aws-aarch64.nix;
-      };
-    };
 }
 
--- modules/hook.nix
+++ modules/hook.nix
@@ -1,29 +1,27 @@
+{ config, pkgs, ... }:
+let
+  kernel-name = config.boot.kernelPackages.kernel.name or "kernel";
+  modulesTree = config.system.modulesTree.override { name = kernel-name + "-modules"; };
+  firmware = config.hardware.firmware;
+  modulesClosure = pkgs.makeModulesClosure {
+    rootModules = [
+      "crypto_aes"
+      "crypto_cbc"
+      "dm-crypt"
+      "scsi-mod"
+      "sd-mod"
+      "virtio-pci"
+      "virtio-scsi"
+    ] ++ config.boot.initrd.kernelModules;
+    kernel = modulesTree;
+    firmware = firmware;
+    allowMissing = false;
+  };
+in
 {
-  imports = [
-    ({ config, pkgs, ... }:
-      let
-        kernel-name = config.boot.kernelPackages.kernel.name or "kernel";
-        modulesTree = config.system.modulesTree.override { name = kernel-name + "-modules"; };
-        firmware = config.hardware.firmware;
-        modulesClosure = pkgs.makeModulesClosure {
-          rootModules = [
-            "crypto_aes"
-            "crypto_cbc"
-            "dm-crypt"
-            "scsi-mod"
-            "sd-mod"
-            "virtio-pci"
-            "virtio-scsi"
-          ] ++ config.boot.initrd.kernelModules;
-          kernel = modulesTree;
-          firmware = firmware;
-          allowMissing = false;
-        };
-      in
-      {
-        config.environment.etc."pickup/kernelmodules".source = modulesClosure;
-      }
-    )
-    nix-netboot-serve.nixosModules.register-nix-store
-  ];
+  config.environment.etc."pickup/kernelmodules".source = modulesClosure;
+
+  boot.postBootCommands = ''
+    PATH=${pkgs.nix}/bin /nix/.nix-netboot-serve-db/register
+  '';
 }
