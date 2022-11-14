b384e856d40ba161668a96f7e3a5b40c593be331
Linus Heckemann <git@sphalerite.org>
2022-11-14 14:59:53 +0100

    AWS stub: add module for fetching EC2 metadata
    This should fix things like SSH access keys in AWS instance user data.

--- modules/stub-aws.nix
+++ modules/stub-aws.nix
@@ -1,7 +1,16 @@
-{ pkgs, ... }: {
+{ pkgs, modulesPath, ... }: {
   boot.initrd.kernelModules = [
     "ena"
     "ixgbevf"
     "nvme"
   ];
+  systemd.services.fetch-ec2-metadata = {
+    wantedBy = [ "multi-user.target" ];
+    before = [ "apply-ec2-data.service" ];
+    script = pkgs.callPackage (modulesPath + "/virtualisation/ec2-metadata-fetcher.nix") {
+      targetRoot = "/";
+      wgetExtraOptions = "";
+    };
+    serviceConfig.Type = "oneshot";
+  };
 }
